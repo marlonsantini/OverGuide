c8f2c5f4aacec83e58c0cc9083d6233056c7f6f1
Marlon Santini <marlonsantini@gmail.com>
2023-05-12 15:19:03 -0300

    refactoring

--- app/src/main/java/fingerfire/com/overwatch/MainActivity.kt
+++ app/src/main/java/fingerfire/com/overwatch/MainActivity.kt
@@ -2,15 +2,54 @@ package fingerfire.com.overwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import fingerfire.com.overwatch.databinding.ActivityMainBinding
 import fingerfire.com.overwatch.view.ui.allcharacters.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, HomeFragment.newInstance())
-                .commitNow()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initNavController()
+    }
+
+    private fun initNavController() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.bottomNavigation.setupWithNavController(navController)
+        onDestinationChanged(navController)
+    }
+
+    private fun onDestinationChanged(navController: NavController) {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+
+            when (destination.id) {
+                R.id.heroesFragment -> {
+                    binding.bottomNavigation.visibility = View.VISIBLE
+                }
+                R.id.mapFragment -> {
+                    binding.bottomNavigation.visibility = View.VISIBLE
+                }
+                R.id.patchsFragment -> {
+                    binding.bottomNavigation.visibility = View.VISIBLE
+                }
+                else -> {
+                    binding.bottomNavigation.visibility = View.GONE
+                }
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/fingerfire/com/overwatch/view/model/Agente.kt
+++
@@ -1,6 +0,0 @@
-package fingerfire.com.overwatch.view.model
-
-
-/** Essa classe é usada para quando recebe alguma alteração da classe model que
- * usa os mesmos campos */
-class Agente
