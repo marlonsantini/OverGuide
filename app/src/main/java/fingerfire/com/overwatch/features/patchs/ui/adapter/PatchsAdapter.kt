b8b5d2f02f5eec4cd8446e7cc61b93ee56c97b08
Marlon Santini <marlonsantini@gmail.com>
2023-05-19 19:25:01 -0300

    implement patchs

--- app/src/main/java/fingerfire/com/overwatch/features/patchs/ui/adapter/PatchsAdapter.kt
+++ app/src/main/java/fingerfire/com/overwatch/features/patchs/ui/adapter/PatchsAdapter.kt
@@ -1,4 +1,34 @@
 package fingerfire.com.overwatch.features.patchs.ui.adapter
 
-class PatchsAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import fingerfire.com.overwatch.databinding.ItemPatchsBinding
+import fingerfire.com.overwatch.features.patchs.data.response.PatchsDataResponse
+
+class PatchsAdapter(
+    private var patchsList: List<PatchsDataResponse>
+) : RecyclerView.Adapter<PatchsAdapter.PatchsViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PatchsViewHolder {
+        return PatchsViewHolder(
+            ItemPatchsBinding
+                .inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: PatchsViewHolder, position: Int) {
+        with(holder) {
+            with(patchsList[position]) {
+
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return patchsList.size
+    }
+
+    class PatchsViewHolder(val binding: ItemPatchsBinding) : RecyclerView.ViewHolder(binding.root)
+
 }
